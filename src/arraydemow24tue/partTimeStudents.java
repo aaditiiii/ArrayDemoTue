ed25f90e2acbeca26e4030dcadb78a8fa9f31619
Aditi Sharma <shar1394@sheridancollege.ca>
2024-01-30 12:47:59 -0500

    added a variable

---
+++ src/arraydemow24tue/partTimeStudents.java
@@ -0,0 +1,20 @@
+package arraydemow24tue;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aditi
+ */
+public class partTimeStudents extends Student{
+    private int numOfCourses;
+
+    public partTimeStudents(int numOfCourses) {
+        this.numOfCourses = numOfCourses;
+    }
+
+   
+}
